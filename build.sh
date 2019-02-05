ef7746a778681605c3825765f8673543b0c7e1f6
zhuzhixian <zzx112587@163.com>
2019-02-05 15:10:25 +0800

    change build and docker-compose file

--- build.sh
+++ build.sh
@@ -2,30 +2,28 @@
 
 echo "构建"
 
-cd server
-cd maven
-./install.sh
+# cd server
+# cd maven
+# ./install.sh
 
-
-cd server/project/distribution/target/docker-bin/boss/Dockerfile
-
-mvn clean package -Pbj -X
+cd server/project
+mvn clean package
 
 cd distribution/target/docker-bin/
 
 cd boss
-docker build -t="boss:latest" .
+docker build -t="openmajiang-boss:latest" .
 cd ../
 #
 cd gateway
-docker build -t="gateway:dev" .
+docker build -t="openmajiang-gateway:latest" .
 cd ../
 #
 cd scene
-docker build -t="scene:dev" .
+docker build -t="openmajiang-scene:latest" .
 cd ../
 
 cd client
-docker build -t="client:dev" .
+docker build -t="openmajiang-client:latest" .
 cd ../
 
